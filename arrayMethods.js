29aaec37f220aea4e4c0046844d3697fb335a758
Djavan Cackley <djavancackley@Djavans-MBP.home>
2018-12-11 22:03:21 -1000

    updated work

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,3 +16,48 @@ var monthNames = ['Jan', 'Feb', 'Mar', 'Apr'];
 var bulkNumbers = [5, 5, 6, 6, 9, 9, 2, 2, 2, 5, 2];
 
 // do your work below!
+
+var nameArray = []
+nameArray.push("Shanon", "Skillman")
+
+for(var i = 6; i<11; i++){
+    genericNumberArray.push(i)
+}
+
+var popResult = colors.pop()
+
+scrambledWords.reverse()
+
+phoneNumber.reverse()
+
+var nextOrder = orderQueue.shift()
+
+mixedNums.sort()
+
+mixedWords.sort()
+
+var notFruit = fruitCollection.splice(1,3)
+
+gemBox.splice(5,2)
+gemBox.splice(6,4)
+
+upToTen.splice(2,0,3,4,5,6,7,8)
+
+brownOnly.splice(1,5)
+brownOnly.splice(2,0,'brown','brown', 'brown')
+
+var orderedValuesNewLength = orderedValues.unshift(1, 2, 3)
+
+var randomThingsArray = genericNumberArray.concat(colors)
+
+var orderQueue2 = [{ takeOut: 'Brownie'}];
+var orderQueue3 = [{ takeOut: 'Icecream'}]
+var updatedOrders = orderQueue.concat(orderQueue2, orderQueue3)
+
+var sentence = scrambledWords.join(' ')
+var myFruits = fruitCollection.join(' + ')
+
+var favoriteFriends = friends.slice(1,3)
+var owesMoney = friends.slice(3)
+
+var monthNameString = 
